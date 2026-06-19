1ba223101030197119d494d39f68967e212352f7
Soniya Bhandari <bhandarisoniya349@gmail.com>
2026-06-19 20:33:28 +0530

    Reposition Personalized Home Care Plans below Home Health Services on mobile viewports

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,17 +1,29 @@
+import React from 'react';
 import Hero from '../../components/Hero/Hero';
 import ServiceGrid from '../../components/ServiceGrid/ServiceGrid';
 import PlansCarousel from '../../components/PlansCarousel/PlansCarousel';
 import EquipmentRental from '../../components/EquipmentRental/EquipmentRental';
 import ConcernsSlider from '../../components/ConcernsSlider/ConcernsSlider';
+import styles from './Home.module.css';
 
 const Home = () => {
   return (
-    <div className="animate-fade-in">
-      <Hero />
-      <PlansCarousel />
-      <ServiceGrid />
-      <ConcernsSlider />
-      <EquipmentRental />
+    <div className={`${styles.homeLayout} animate-fade-in`}>
+      <div className={styles.sectionHero}>
+        <Hero />
+      </div>
+      <div className={styles.sectionPlans}>
+        <PlansCarousel />
+      </div>
+      <div className={styles.sectionServices}>
+        <ServiceGrid />
+      </div>
+      <div className={styles.sectionConcerns}>
+        <ConcernsSlider />
+      </div>
+      <div className={styles.sectionEquipment}>
+        <EquipmentRental />
+      </div>
     </div>
   );
 };
--- frontend/src/pages/Services/Services.jsx
+++ frontend/src/pages/Services/Services.jsx
@@ -5,16 +5,29 @@ import WhyChooseUs from '../../components/WhyChooseUs/WhyChooseUs';
 import PlansCarousel from '../../components/PlansCarousel/PlansCarousel';
 import EquipmentRental from '../../components/EquipmentRental/EquipmentRental';
 import ConcernsSlider from '../../components/ConcernsSlider/ConcernsSlider';
+import styles from './Services.module.css';
 
 const Services = () => {
   return (
-    <div className="animate-fade-in">
-      <Hero />
-      <PlansCarousel />
-      <ServiceGrid />
-      <ConcernsSlider />
-      <EquipmentRental />
-      <WhyChooseUs />
+    <div className={`${styles.servicesLayout} animate-fade-in`}>
+      <div className={styles.sectionHero}>
+        <Hero />
+      </div>
+      <div className={styles.sectionPlans}>
+        <PlansCarousel />
+      </div>
+      <div className={styles.sectionServices}>
+        <ServiceGrid />
+      </div>
+      <div className={styles.sectionConcerns}>
+        <ConcernsSlider />
+      </div>
+      <div className={styles.sectionEquipment}>
+        <EquipmentRental />
+      </div>
+      <div className={styles.sectionWhyChooseUs}>
+        <WhyChooseUs />
+      </div>
     </div>
   );
 };
